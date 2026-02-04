1478c0fb95115dc3406c1b55e3151cfdd516c06c
Carlos Chavoya <carlos.chavoya4676@alumnos.udg.mx>
2026-02-03 19:35:08 -0600

    Se agrega un thead a la vista de la orden de compra

--- resources/views/formato_orden_compra.blade.php
+++ resources/views/formato_orden_compra.blade.php
@@ -51,61 +51,69 @@
 <body>
 
 <table>
-    <tr>
-        <td rowspan="2" class="center header">
-            Deconovo<br>
-            Recubrimientos
-        </td>
-        <td class="center">Fecha: {{$orden_compra->fecha_generada->format('d/m/Y')}}</td>
-        <td class="center bold">ORDEN DE COMPRA</td>
-        <td class="center bold">Folio: {{str_pad($orden_compra->id, 3, "0", STR_PAD_LEFT)}}</td>
-    </tr>
-    <tr>
-        <td class="center bold">Tienda Arbolada</td>
-        <td colspan="2" class="provider center">Proveedor: {{strtoupper($proveedor->nombre)}}</td>
-    </tr>
-
-    <tr class="center bold">
-        <td>Cantidad</td>
-        <td>Unidad</td>
-        <td>Precio</td>
-        <td>Concepto</td>
-    </tr>
-    @foreach ($productos as $producto)
-        <tr class="center">
-            <td>{{$producto['pedir']}}</td>
-            <td>{{$producto['unidad']}}</td>
-            <td></td>
-            <td>{{$producto['producto']}}</td>
+    <thead>
+        <tr>
+            <td rowspan="2" class="center header">
+                Deconovo<br>Recubrimientos
+            </td>
+            <td class="center">
+                Fecha: {{ $orden_compra->fecha_generada->format('d/m/Y') }}
+            </td>
+            <td class="center bold">ORDEN DE COMPRA</td>
+            <td class="center bold">
+                Folio: {{ str_pad($orden_compra->id, 3, "0", STR_PAD_LEFT) }}
+            </td>
         </tr>
-    @endforeach
-    
-    <tr>
-        <td colspan="4" class="big-box"></td>
-    </tr>
+        <tr>
+            <td class="center bold">Tienda Arbolada</td>
+            <td colspan="2" class="provider center">
+                Proveedor: {{ strtoupper($proveedor->nombre) }}
+            </td>
+        </tr>
+        <tr class="center bold">
+            <td>Cantidad</td>
+            <td>Unidad</td>
+            <td>Precio</td>
+            <td>Concepto</td>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($productos as $producto)
+            <tr class="center">
+                <td>{{$producto['pedir']}}</td>
+                <td>{{$producto['unidad']}}</td>
+                <td></td>
+                <td>{{$producto['producto']}}</td>
+            </tr>
+        @endforeach
+        
+        <tr>
+            <td colspan="4" class="big-box"></td>
+        </tr>
 
-    <tr>
-        <td colspan="2">
-            <b>Recomendación para Almacén:</b><br><br>
+        <tr>
+            <td colspan="2">
+                <b>Recomendación para Almacén:</b><br><br>
 
-            <b>Facturar:</b> Ana Livier<br>
-            <b>Uso de CFDI:</b> G01 Adquisición de Mercancías<br>
-            <b>Forma de Pago:</b> 99 Por Definir<br>
-            <b>Método de pago:</b> PPD Pago en Parcialidades o Diferidos<br><br>
+                <b>Facturar:</b> Ana Livier<br>
+                <b>Uso de CFDI:</b> G01 Adquisición de Mercancías<br>
+                <b>Forma de Pago:</b> 99 Por Definir<br>
+                <b>Método de pago:</b> PPD Pago en Parcialidades o Diferidos<br><br>
 
-            <span class="highlight">Recepción de mercancía</span>
-            <p>Lunes a Viernes 9:30 am. - 12:45 pm. </p>
-            <p> 14:00 pm. - 17:30 pm.</p>
-        </td>
+                <span class="highlight">Recepción de mercancía</span>
+                <p>Lunes a Viernes 9:30 am. - 12:45 pm. </p>
+                <p> 14:00 pm. - 17:30 pm.</p>
+            </td>
 
-        <td colspan="2" class="center">
-            <b>Atentamente:</b><br><br><br>
-            {{$nombre}}<br><br>
-            <b>Favor de Enviar Factura al correo:</b>
-            <br><u><a href="mailto:{{$correo}}">{{$correo}}</a></u>
-            
-        </td>
-    </tr>
+            <td colspan="2" class="center">
+                <b>Atentamente:</b><br><br><br>
+                {{$nombre}}<br><br>
+                <b>Favor de Enviar Factura al correo:</b>
+                <br><u><a href="mailto:{{$correo}}">{{$correo}}</a></u>
+                
+            </td>
+        </tr>
+    </tbody>
 </table>
 
 </body>
