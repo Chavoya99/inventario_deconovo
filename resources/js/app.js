db29cc796eaf87ed774f0e1a2d229c7224589aa9
Carlos Chavoya <carlos.chavoya4676@alumnos.udg.mx>
2026-02-11 05:41:18 -0600

    Librería toastr y actualización de vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,15 @@
 import './bootstrap';
 
+import toastr from 'toastr';
+import 'toastr/build/toastr.min.css';
 
+window.toastr = toastr;
+
+toastr.options = {
+    closeButton: false,
+    progressBar: false,
+    timeOut: 3000,
+    extendedTimeOut: 1000,
+    positionClass: "toast-top-right",
+    escapeHtml: false,
+}
