22051a395c2e28d1bc1d1bc55031bccca0630cee
Carlos Chavoya <carlos.chavoya4676@alumnos.udg.mx>
2026-02-11 05:40:36 -0600

    Nueva función para incluir productos en la lista que se enviará a la orden de compra

--- resources/views/components/⚡revisar-reporte-tabla.blade.php
+++ resources/views/components/⚡revisar-reporte-tabla.blade.php
@@ -1,67 +1,330 @@
 <?php
 
+use App\Models\Producto;
 use Livewire\Component;
-use Livewire\Attributes\On;
 use App\Models\ReporteFaltante;
 
 new class extends Component
 {
-    public $productos, $reporte, $reporte_id;
+    public $reporte, $reporte_id;
+    public $precios = [], $productos = [], $productos_cero = [], $nombres_productos = [];
+    public $pedir_modificado = [];
+    public $seleccionados = [];
+    public $precios_venta = [];
 
-    //protected $listeners = ['productoEliminado'];
+    public function mount()
+    {
+        $this->cargarProductos();
 
-    public function mount($reporte_id)
-    {   
-        $this->reporte = ReporteFaltante::with('productos')->find($reporte_id);
-        $this->productos = $this->reporte->productos;
-    }
-
-    #[On('productoEliminado')]
-    public function productoEliminado(){
-        // $this->refreshComponent();
-        // $this->reporte = ReporteFaltante::with('productos')->find($this->reporte_id);
-        // $this->productos = $this->reporte
-        // ->productos()
-        // ->withPivot('existencia')
-        // ->get();
-        $this->reporte = ReporteFaltante::with([
-        'productos' => function ($q) {
-            $q->withPivot('existencia', 'pedir_modificado');
+        foreach ($this->productos as $producto) {
+            $this->precios[$producto->id] = $producto->precio_proveedor;
+            $this->precios_venta[$producto->id] = $producto->precio_venta;
+            $this->pedir_modificado[$producto->id] = $producto->pivot->pedir_modificado;
+            $this->nombres_productos[$producto->id] = $producto->producto;
         }
-    ])->find($this->reporte_id);
 
-    $this->productos = $this->reporte->productos;
+        foreach ($this->productos_cero as $producto) {
+            $this->precios[$producto->id] = $producto->precio_proveedor;
+            $this->precios_venta[$producto->id] = $producto->precio_venta;
+            $this->pedir_modificado[$producto->id] = $producto->pivot->pedir_modificado;
+            $this->nombres_productos[$producto->id] = $producto->producto;
+        }
     }
 
-    
+    public function cargarProductos()
+    {
+        //$reporte = ReporteFaltante::find($this->reporte_id);
+            
+        //Filtrar productos con columna incluir igual a 1
+        $this->productos = $this->reporte->productos->filter(function ($producto){
+            return $producto->pivot->incluir != 0;
+        });
+        
+        //Filtrar productos con columna incluir igual a 0
+        $this->productos_cero = $this->reporte->productos->filter(function ($producto){
+            return $producto->pivot->incluir == 0;
+        })->sortByDesc(function($producto){
+            return $producto->producto && $producto->pivot->pedir_registrado;
+        });
 
+    }
+
+    public function incluir_producto_reporte($producto_id)
+    {   
+        $producto = $this->reporte->productos()->where('producto_id', $producto_id)->first();
+        $producto->reportes()->where('reporte_id', $this->reporte_id)->update(['pedir_modificado' => $producto->pivot->pedir_registrado,'incluir' => 1]);
+
+        $this->precios[$producto_id] = $producto->precio_proveedor;
+        $this->precios_venta[$producto_id] = $producto->precio_venta;
+        $this->pedir_modificado[$producto_id] = $producto->pivot->pedir_modificado;
+        $this->nombres_productos[$producto->id] = $producto->producto;
+
+        $this->cargarProductos();
+    }
+
+    public function eliminar_producto_reporte($producto_id)
+    {   
+        if (count($this->productos) <= 1) {
+
+            $this->dispatch('error',
+                message: 'No puedes eliminar el último producto del reporte.'
+            );
+            $this->cargarProductos();
+            return;
+        }else{
+            $producto = $this->reporte->productos()->where('producto_id', $producto_id)->first();
+            $producto->reportes()->where('reporte_id', $this->reporte_id)->update(['incluir' => 0]);
+            
+            unset($this->precios[$producto_id]);
+            unset($this->precios_venta[$producto_id]);
+            unset($this->pedir_modificado[$producto_id]);
+            unset($this->seleccionados[$producto_id]);
+            unset($this->nombres_productos[$producto->id]);
+
+            $this->cargarProductos();
+        }
+        
+    }
 
 };
 ?>
-
-<table class="w-full text-sm text-left text-gray-700">
-        <thead class="bg-sky-400 text-black border-b">
-            <tr>
-                <th class="px-6 py-3 font-medium">Producto</th>
-                <th class="px-6 py-3 font-medium">Unidad</th>
-                <th class="px-6 py-3 font-medium">Máximo</th>
-                <th class="px-6 py-3 font-medium">Existencia</th>
-                <th class="px-6 py-3 font-medium">Pedir</th>
-                <th class="px-6 py-3 font-medium">Precio venta</th>
-                <th class="px-6 py-3 font-medium">Precio proveedor</th>
-                @if (auth()->user()->isAdmin())
-                    <th class="px-6 py-3 text-right font-medium">Acciones</th>
-                @endif
+<div>
+    <h3 class="font-semibold text-2xl text-gray-800 leading-tight text-center ">
+        {{ __('Incluir') }}
+    </h3>
+    <div class="overflow-x-auto bg-white rounded-lg shadow border border-gray-200">
+        
+        <table class="w-full text-sm text-left text-gray-700">
+                <thead class="bg-sky-400 text-black border-b">
+                    <tr>
+                        <th class="px-6 py-3 font-medium">Producto</th>
+                        <th class="px-6 py-3 font-medium">Unidad</th>
+                        <th class="px-6 py-3 font-medium">Máximo</th>
+                        <th class="px-6 py-3 font-medium">Existencia</th>
+                        <th class="px-6 py-3 font-medium">Pedir</th>
+                        <th class="px-6 py-3 font-medium">Precio proveedor</th>
+                        <th class="px-6 py-3 font-medium">Precio venta</th>
+                        @if (auth()->user()->isAdmin())
+                            <th class="px-6 py-3 text-right font-medium">Acciones</th>
+                        @endif
+                        
+                    
+                    </tr>
+                </thead>
                 
+                <tbody>
+                
+                @foreach ($productos as $producto)
+                    <tr 
+                        wire:key="producto-{{ $producto->id }}" 
+                        class="border-b hover:bg-gray-50"
+                    >   
+                        @if($producto->pivot->registrado != 1 && $reporte->status == 'aprobado' )
+                            <td class="px-6 py-4 font-medium text-gray-900 min-w-[450px]">
+                                <input  
+                                    wire:model.defer="nombres_productos.{{ $producto->id }}" 
+                                    wire:key="producto-{{ $producto->id }}" 
+                                    type="text" 
+                                    value="{{ $producto->producto }}"
+                                    class="w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring-blue-500 text-sm"
+                                    >
+                            </td>
+                        @else
+                            <td class="px-6 py-4">
+                                {{$producto->producto}}    
+                            </td>
+                        @endif
+                        
+                        <td class="px-6 py-4">
+                            {{$producto->unidad}}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{$producto->maximo}}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{$producto->pivot->existencia}}
+                        </td>
+                        @if($producto->pivot->registrado != 1 && $reporte->status == 'aprobado' )
+                            <td class="px-6 py-4 min-w-[100px]">
+                                <input  wire:model.defer="pedir_modificado.{{ $producto->id }}" 
+                                wire:key="producto-{{ $producto->id }}" 
+                                type="number" step="1" min="1" value="{{$producto->pivot->pedir_modificado}}"
+                                class="w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring-blue-500"
+                                max="{{$producto->maximo - $producto->pivot->existencia}}">
+                                
+                            </td>
+
+                            <td class="px-6 py-4 min-w-[100px]">
+                                <input  wire:model.defer="precios.{{ $producto->id }}" 
+                                wire:key="producto-{{ $producto->id }}" 
+                                type="number" step="0.01" min="0" value="{{$producto->precio_proveedor}}"
+                                class="w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring-blue-500"
+                                max="999999">
+                            </td>
+
+                            <td class="px-6 py-4 min-w-[100px]">
+                                <input  wire:model.defer="precios_venta.{{ $producto->id }}" 
+                                wire:key="producto-{{ $producto->id }}" 
+                                type="number" step="0.01" min="0" value="{{$producto->precio_venta}}"
+                                class="w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring-blue-500"
+                                max="999999">
+                            </td>
+                        @else
+                            <td class="px-6 py-4">
+                                {{$producto->pivot->pedir_modificado}}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{$producto->precio_proveedor}}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{$producto->precio_venta}}
+                            </td>
+
+                        @endif
+                
+                        <!-- ACCIONES -->
+                        <td class="px-6 py-4">
+                            @if(auth()->user()->isAdmin())
+                                <div class="flex justify-end items-center gap-4 text-sm">
+
+                                    @if($producto->pivot->registrado)
+                                        <div class="max-w-sm w-full space-y-3">
+                                            <input checked disabled type="checkbox" class="shrink-0 size-4 bg-transparent border-line-3 rounded-sm shadow-2xs text-primary focus:ring-0 focus:ring-offset-0 checked:bg-primary-checked checked:bg-green-500 disabled:opacity-50 disabled:pointer-events-none" id="hs-default-checkbox">
+                                        </div>
+                                    @else
+                                        <div class="max-w-sm w-full space-y-3">
+                                            <input wire:model="seleccionados.{{ $producto->id }}" type="checkbox" class="shrink-0 size-4 bg-transparent border-line-3 rounded-sm shadow-2xs text-primary focus:ring-0 focus:ring-offset-0 checked:bg-primary-checked checked:border-primary-checked disabled:opacity-50 disabled:pointer-events-none" id="hs-default-checkbox">
+                                        </div>
+                                    @endif
+                                    
+
+                                    <!-- Eliminar -->
+                                    @if ($producto->pivot->registrado == 1)
+                                        <i class="fa-solid fa-ban"></i>
+                                    @else
+                                        <button 
+                                            wire:key="producto-{{ $producto->id }}"
+                                            onclick="confirm('¿Estas seguro de continuar?')
+                                            || event.stopImmediatePropagation()"
+                                            wire:click="eliminar_producto_reporte({{$producto->id}})"
+                                            class="text-red-600 hover:text-red-800"
+                                            title="Quitar">
+                                            <i class="fa-solid fa-circle-minus"></i>
+                                        </button>
+                                    @endif
+                                    
+                                </div>
+                            @endif
+                        </td>
+                    </tr>
+                @endforeach           
+                
+            </tbody>
             
-            </tr>
-        </thead>
-        
-        <tbody>
-        
-        @foreach ($productos as $producto)
-            <livewire-reporte-producto-estado :producto_id="$producto->id" :reporte_id="$reporte->id" :producto=$producto />
-        @endforeach           
-        
-    </tbody>
-</table>
+        </table>
+    </div>
+    <br>
+    <hr class="border-gray-500 dark:border-neutral-500">
+    <br>
+    <h3 class="font-semibold text-2xl text-gray-800 leading-tight text-center ">
+        {{ __('No incluir') }}
+    </h3>
+    <div class="overflow-x-auto bg-white rounded-lg shadow border border-gray-200">
+        <table class="w-full text-sm text-left text-gray-700">
+                @if ($productos_cero->isEmpty())
+                    <tr>
+                        <td colspan="3" class="px-6 py-4 text-center text-gray-500">
+                            No hay productos registrados
+                        </td>
+                    </tr>
+                @else
+                    <thead class="bg-sky-400 text-black border-b">
+                        <tr>
+                            <th class="px-6 py-3 font-medium">Producto</th>
+                            <th class="px-6 py-3 font-medium">Unidad</th>
+                            <th class="px-6 py-3 font-medium">Máximo</th>
+                            <th class="px-6 py-3 font-medium">Existencia</th>
+                            <th class="px-6 py-3 font-medium">Pedir</th>
+                            <th class="px-6 py-3 font-medium">Precio proveedor</th>
+                            <th class="px-6 py-3 font-medium">Precio venta</th>
+                            @if (auth()->user()->isAdmin())
+                                <th class="px-6 py-3 text-right font-medium">Acciones</th>
+                            @endif
+                            
+                        
+                        </tr>
+                    </thead>
+                    
+                    <tbody>
+                    
+                    
+                    @foreach ($productos_cero as $producto)
+                        <tr 
+                            wire:key="producto-{{ $producto->id }}" 
+                            class="border-b hover:bg-gray-50"
+                        >   
+                            <td id=nombre class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                {{$producto->producto}}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{$producto->unidad}}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{$producto->maximo}}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{$producto->pivot->existencia}}
+                            </td>
+                            
+                            <td class="px-6 py-4">
+                                {{$producto->pivot->pedir_modificado}}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{$producto->precio_proveedor}}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{$producto->precio_venta}}
+                            </td>
+
+                    
+                            <!-- ACCIONES -->
+                            <td class="px-6 py-4">
+                                @if(auth()->user()->isAdmin())
+                                    <div class="flex justify-end items-center gap-4 text-sm">
+
+                                        <!--Incluir -->
+                                        @if ($producto->pivot->registrado == 1)
+                                            <i class="fa-solid fa-ban"></i>
+                                        @else
+                                            @if($producto->pivot->pedir_registrado == 0)
+                                                <button 
+                                                    wire:key="producto-{{ $producto->id }}"
+                                                    wire:click="incluir_producto_reporte({{$producto->id}})"
+                                                    class="text-dark-600 hover:text-dark-800 block mx-auto"
+                                                    title="No permitido">
+                                                    <i class="fa-solid fa-circle-stop"></i>
+                                                </button>
+                                            @else
+                                                <button 
+                                                    wire:key="producto-{{ $producto->id }}"
+                                                    wire:click="incluir_producto_reporte({{$producto->id}})"
+                                                    class="text-green-600 hover:text-green-800 block mx-auto"
+                                                    title="Incluir">
+                                                    <i class="fa-solid fa-circle-plus"></i>
+                                                </button>
+                                            @endif
+                                            
+                                        @endif
+                                        
+                                    </div>
+                                @endif
+                            </td>
+                        </tr>
+                    @endforeach
+                @endif         
+                
+            </tbody>
+            
+        </table>
+    </div>
+</div>
