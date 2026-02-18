7d2d6570380f8f178b76fe403632f5429841fbaa
Carlos Chavoya <carlos.chavoya4676@alumnos.udg.mx>
2026-02-18 14:14:11 -0600

    Transición de modal para comentarios sin duración especifica

--- resources/views/components/⚡orden-estado-fila.blade.php
+++ resources/views/components/⚡orden-estado-fila.blade.php
@@ -191,10 +191,10 @@ new class extends Component
         <div
             x-data="{ open: @entangle('mostrarModalComentarios') }"
             x-show="open"
-            x-transition:enter="transition-all ease-out duration-300"
+            x-transition:enter="transition-all ease-out"
             x-transition:enter-start="opacity-0 scale-95 translate-y-2"
             x-transition:enter-end="opacity-100 scale-100 translate-y-0"
-            x-transition:leave="transition-all ease-in duration-200"
+            x-transition:leave="transition-all ease-in"
             x-transition:leave-start="opacity-100 scale-100 translate-y-0"
             x-transition:leave-end="opacity-0 scale-95 translate-y-2"
             class="fixed inset-0 z-50 flex items-center justify-center">
